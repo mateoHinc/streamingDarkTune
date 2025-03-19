0941ea242267b3848a0da4b71e10b32b42cf4be5
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-03-18 21:41:39 -0500

    Configure palette of colors and typography

--- streamingDarkTune/src/App.jsx
+++ streamingDarkTune/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="flex flex-col justify-center items-center bg-primary min-h-screen font-sans text-white">
+        <h1 className="mb-4 font-heading text-secondary text-4xl">
+          Bienvenido a Dark Tune
+        </h1>
+        <p className="mb-6 text-gray-300 text-lg">
+          Descubre tu género musical favorito
         </p>
+        <button className="bg-secondary hover:bg-accentHover px-6 py-3 rounded-lg font-semibold text-white transition">
+          Explorar
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- streamingDarkTune/tailwind.config.js
+++ streamingDarkTune/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1e1b4b", // Indigo oscuro (fondo)
+        secondary: "#7c3aed", // Vivid Violet (acentos)
+        accent: "#c084fc", // Lavender
+        accentHover: "#a855f7", // Hover para botones
+        background: "#f3f4f6", // Fondo claro (modo claro)
+        textPrimary: "#1f2937", // Gris oscuro para texto
+      },
+      fontFamily: {
+        fontFamily: {
+          sans: ["Inter", "sans-serif"], // Texto normal
+          heading: ["Montserrat", "sans-serif"], // Títulos
+        },
+      },
+    },
   },
   plugins: [],
 };
