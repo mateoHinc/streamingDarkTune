825664afd54300124c993f140867046f4e574055
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2025-03-29 17:25:20 -0500

    Add visual background at section review user

--- streamingDarkTune/src/components/UserReviewsSection.jsx
+++ streamingDarkTune/src/components/UserReviewsSection.jsx
@@ -25,41 +25,54 @@ function StarRating({ rating }) {
 
 function UserReviewsSection() {
   return (
-    <section className="bg-primary py-16 min-h-screen text-white text-center">
-      <h2 className="mb-10 font-bold text-3xl md:text-4xl">
-        Reseñas de Usuarios
-      </h2>
-      <div className="mx-auto px-4 max-w-6xl">
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={20}
-          pagination={{ clickable: true }}
-          breakpoints={{
-            480: { slidesPerView: 1, spaceBetween: 20 },
-            640: { slidesPerView: 2, spaceBetween: 20 },
-            1024: { slidesPerView: 3, spaceBetween: 30 },
-          }}
-          modules={[Pagination]}
-        >
-          {userReviews.map((user) => (
-            <SwiperSlide key={user.id} className="flex h-full">
-              <div className="flex flex-col justify-between items-center bg-secondary shadow-lg p-6 rounded-xl w-full min-h-[350px] text-left">
-                <img
-                  src={user.photo}
-                  alt={user.name}
-                  className="shadow-md mb-4 rounded-full ring-2 ring-white ring-offset-2 w-20 sm:w-24 md:w-28 h-20 sm:h-24 md:h-28 object-cover"
-                />
-                <h3 className="mb-2 font-semibold text-base sm:text-lg md:text-xl">
-                  {user.name}
-                </h3>
-                <p className="mb-4 text-gray-300 sm:text-base text-lg text-center">
-                  {user.review}
-                </p>
-                <StarRating rating={user.rating} />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+    <section className="relative overflow-hidden text-white text-center">
+      <video
+        autoPlay
+        loop
+        playsInline
+        className="top-0 left-0 z-0 absolute w-full h-full object-cover"
+      >
+        <source src="/src/assets/videoMusicLoop.mp4" type="video/mp4" />
+      </video>
+
+      <div className="z-10 absolute inset-0 bg-black bg-opacity-60"></div>
+
+      <div className="z-20 relative px-4 py-16 min-h-screen">
+        <h2 className="mb-10 font-bold text-3xl md:text-4xl">
+          Reseñas de Usuarios
+        </h2>
+        <div className="mx-auto px-4 max-w-6xl">
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={20}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              480: { slidesPerView: 1, spaceBetween: 20 },
+              640: { slidesPerView: 2, spaceBetween: 20 },
+              1024: { slidesPerView: 3, spaceBetween: 30 },
+            }}
+            modules={[Pagination]}
+          >
+            {userReviews.map((user) => (
+              <SwiperSlide key={user.id} className="flex h-full">
+                <div className="flex flex-col justify-between items-center bg-secondary shadow-lg p-6 rounded-xl w-full min-h-[350px] text-left">
+                  <img
+                    src={user.photo}
+                    alt={user.name}
+                    className="shadow-md mb-4 rounded-full ring-2 ring-white ring-offset-2 w-28 sm:w-24 md:w-28 h-28 sm:h-24 md:h-28 object-cover"
+                  />
+                  <h3 className="mb-2 font-semibold sm:text-lg md:text-xl text-2xl">
+                    {user.name}
+                  </h3>
+                  <p className="mb-4 text-gray-300 sm:text-base text-lg text-center">
+                    {user.review}
+                  </p>
+                  <StarRating rating={user.rating} />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </section>
   );
